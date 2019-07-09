2227780438d19f2d8e42401a7d1975ae599f548c
CCodingMan <2693986772@qq.com>
2019-07-09 22:23:13 +0800

    配置SpringBoot支持自动装载Servlet

---
+++ springboot_servlet/src/main/java/com/ljj/springboot_servlet/servlet/ServletConfiguration.java
@@ -0,0 +1,14 @@
+package com.ljj.springboot_servlet.servlet;
+
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Auther: ljj
+ * @Date: 2019/7/9 21:27
+ * @Description:
+ */
+@Configuration
+@ServletComponentScan
+public class ServletConfiguration {
+}
---
+++ springboot_servlet/src/main/java/com/ljj/springboot_servlet/servlet/TestServlet.java
@@ -0,0 +1,26 @@
+package com.ljj.springboot_servlet.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @Auther: ljj
+ * @Date: 2019/7/9 21:20
+ * @Description:
+ */
+@WebServlet(urlPatterns = "/test")
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("utf-8");
+        PrintWriter writer = resp.getWriter();
+        writer.write("执行TestServlet成功");
+        writer.close();
+    }
+}
